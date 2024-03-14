2b3a354ba53eb4815025a54d9ffa44fc78ff6828
Josinaldo Junior <x17651@rn.senac.local>
2024-03-14 10:02:22 -0300

    Finish the issue 1

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import http from "http";
 
 const PORT = 3000; 
+const rotas = {
+    "/":"Rota Home!",
+    "/livros": "Rota Livros!",
+    "/autores": "Rota Autores!"
+}
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"content.type":"text/plan"});
-    res.end("Hello World!");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () =>{
